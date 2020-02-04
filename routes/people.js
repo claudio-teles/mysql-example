9c6fd5afced360e5132cdb26ceec97cb852865f5
claudio-teles <claudio_teles@yahoo.com>
2020-02-04 09:44:02 -0300

    Commit Quatro.

--- routes/people.js
+++ routes/people.js
@@ -3,11 +3,25 @@ const Router = express.Router();
 
 const mysqlConnection = require("../connection");
 
+var ocultaSenha = (senha) => {
+  var senha_oculta = '';
+  for (var i = 0; i < String(senha).length; i++) {
+    senha_oculta += '*';
+  }
+  return senha_oculta;
+}
+
 Router.post("/", (req, res)=>{
-  var create = "INSERT INTO `people`(`name`, `age`) VALUES ('"+req.body.name+"', '"+req.body.age+"')";
+  var create = "INSERT INTO people (nome, senha) VALUES('"+req.body.nome+"', '"+req.body.senha+"')";
   mysqlConnection.query(create, (err, rows, fields)=>{
     if (!err) {
-      res.status(201).send(rows);
+      req.session.nome = req.body.nome;
+      req.session.senha = req.body.senha;
+      var resposta = {
+        nome: req.session.nome,
+        senha: ocultaSenha(req.session.senha)
+      }
+      res.status(201).send(resposta);
     } else {
       console.log(err);
     }
@@ -16,9 +30,18 @@ Router.post("/", (req, res)=>{
 
 Router.get("/", (req, res)=>{
   var read = "SELECT * FROM people ORDER BY id ASC";
+  var resposta = [];
   mysqlConnection.query(read, (err, rows, fields)=>{
     if (!err) {
-      res.status(200).send(rows);
+      for (var i = 0; i < rows.length; i++) {
+        var json = {
+          id: rows[i].id,
+          nome: rows[i].nome,
+          senha: ocultaSenha(rows[i].senha)
+        }
+        resposta.push(json);
+      }
+      res.status(200).send(resposta);
     } else {
       console.log(err);
     }
@@ -26,10 +49,17 @@ Router.get("/", (req, res)=>{
 });
 
 Router.get("/:id", (req, res)=>{
-  var read_people_id = "SELECT * FROM `people` WHERE id = "+req.params.id;
-  mysqlConnection.query(read_people_id, (err, rows, fields)=>{
+  var read_people_by_id = "SELECT * FROM people WHERE id = "+req.params.id;
+  var resposta = {}
+  mysqlConnection.query(read_people_by_id, (err, rows, fields)=>{
     if (!err) {
-      res.status(200).send(rows);
+      var json = {
+        id: rows[0].id,
+        nome: rows[0].nome,
+        senha: ocultaSenha(rows[0].senha)
+      }
+      resposta = json;
+      res.status(200).send(resposta);
     } else {
       console.log(err);
     }
@@ -37,7 +67,7 @@ Router.get("/:id", (req, res)=>{
 });
 
 Router.put("/:id", (req, res)=>{
-  var update_by_id = "UPDATE `people` SET `name` = '"+req.body.name+"', `age` = '"+req.body.age+"' WHERE id = "+req.params.id;
+  var update_by_id = "UPDATE people SET nome = '"+req.body.nome+"', senha = '"+req.body.senha+"' WHERE id = "+req.params.id;
   mysqlConnection.query(update_by_id, (err, rows, fields)=>{
     if (!err) {
       res.status(200).send(rows);
@@ -48,7 +78,7 @@ Router.put("/:id", (req, res)=>{
 });
 
 Router.delete("/:id", (req, res)=>{
-  var delete_people_by_id = "DELETE FROM `people` WHERE id = "+req.params.id;
+  var delete_people_by_id = "DELETE FROM people WHERE id = "+req.params.id;
   mysqlConnection.query(delete_people_by_id, (err, rows, fields)=>{
     if (!err) {
       res.status(200).send(rows);
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const session = require("express-session");
 const bodyParser = require("body-parser");
 
 const PeopleRoutes = require("./routes/people");
@@ -6,8 +7,12 @@ const PeopleRoutes = require("./routes/people");
 var app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+  secret: 'node_express_session',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
 }));
 app.use("/people", PeopleRoutes);
 
